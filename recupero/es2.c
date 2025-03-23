848ae3d51cee2945a8843e5b94e6dffd005373ca
Lorenzodmascio <156659155+Lorenzodmascio@users.noreply.github.com>
2025-03-23 13:14:48 +0000

    upd4

---
+++ recupero/es2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#define N 10
+
+typedef struct persona
+{
+    char cognome[20];
+    int voti[N];
+}persona;
+
+
+void carica (Persona St[]); 
+void stampa(Persona St[]);  
+int visualizzaMedia(Persona St[ ], char cog[ ]);
+int votoMassimo(Persona St[]);  
+int indiceVotoMassimo (Persona St[]); 
+
+int main();
+{   
+    
+}
+
+    
+void carica (Persona St[])
+{
+    
+}
